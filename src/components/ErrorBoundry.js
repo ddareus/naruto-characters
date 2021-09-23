b342367c9840b0829fa2b679ce971a44ff21b086
Daniel Daréus <dareusdaniel@gmail.com>
2021-09-23 19:33:00 -0400

    Create ErrorBoundry.js

---
+++ src/components/ErrorBoundry.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import { characters } from '../characters';
+import Shippuden from '../img/naruto-shippuden.png';
+import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry';
+
+class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			characters: [],
+			searchfield: '',
+		};
+	}
+
+	componentDidMount() {
+		this.setState({ characters: characters });
+	}
+
+	onSearchChange = (event) => {
+		this.setState({ searchfield: event.target.value });
+	};
+	render() {
+		const { characters, searchfield } = this.state;
+		const filteredCharacters = characters.filter((character) => {
+			return character.name.toLowerCase().includes(searchfield.toLowerCase());
+		});
+		return !characters.length ? (
+			<h1 className='tc'>Loading...</h1>
+		) : (
+			<div className='tc'>
+				<img src={Shippuden} alt='' weight='200' height='103' />
+				<SearchBox searchChange={this.onSearchChange} />
+				<Scroll>
+					<ErrorBoundry>
+						<CardList characters={filteredCharacters} />
+					</ErrorBoundry>
+				</Scroll>
+			</div>
+		);
+	}
+}
+
+export default App;
